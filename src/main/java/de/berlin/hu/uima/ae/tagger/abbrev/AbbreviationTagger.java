058dbda84d437512e1b37c96de52801378c17e0d
thuber@informatik.hu-berlin.de <thuber@informatik.hu-berlin.de>
2012-09-27 19:08:33 +0200

    ABBREV tagger will only tag all abbreviations in a PubMed document (if there are any) rather than the whole text

--- src/main/java/de/berlin/hu/uima/ae/tagger/abbrev/AbbreviationTagger.java
+++ src/main/java/de/berlin/hu/uima/ae/tagger/abbrev/AbbreviationTagger.java
@@ -1,5 +1,6 @@
 package de.berlin.hu.uima.ae.tagger.abbrev;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,8 +9,10 @@ import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.u_compare.shared.semantic.chemical.Chemical;
+import org.uimafit.util.JCasUtil;
 
 import de.berlin.hu.chemspot.Mention;
+import de.berlin.hu.types.PubmedDocument;
 import de.berlin.hu.util.Constants;
 
 public class AbbreviationTagger extends JCasAnnotator_ImplBase {
@@ -23,19 +26,33 @@ public class AbbreviationTagger extends JCasAnnotator_ImplBase {
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		String text = aJCas.getDocumentText();
 		List<Mention> abbreviations = abbrevTagger.getMentions(text);
+		Iterator<PubmedDocument> pmIterator =  JCasUtil.iterator(aJCas, PubmedDocument.class);
+		PubmedDocument currDocument = pmIterator.hasNext() ? pmIterator.next() : null;
 		
 		for (Mention abbr : abbreviations) {
 			if (abbr.getText().length() < 2) {
 				continue;
-			}
+			} 
 			
 			//createAbbreviationAnnotation(aJCas, abbr.getStart(), abbr.getEnd(), abbr.getId());
+			while (pmIterator.hasNext() && currDocument.getEnd() < abbr.getStart()) {
+				currDocument = pmIterator.next();
+			}
+			
+			int offset = -1;
+			if (currDocument != null) {
+				text = currDocument.getCoveredText();
+				offset = currDocument.getBegin();
+			} else {
+				text = aJCas.getDocumentText();
+				offset = 0;
+			}
 			
 			Pattern pattern = Pattern.compile("(?<!\\p{Alpha})" + Pattern.quote(abbr.getText()) + "(?!\\p{Alpha})");
 			Matcher matcher = pattern.matcher(text);
 			while (matcher.find()) {
-				int begin = matcher.start();
-				int end = matcher.end();
+				int begin = matcher.start() + offset;
+				int end = matcher.end() + offset;
 				
 				createAbbreviationAnnotation(aJCas, begin, end, abbr.getCHID());
 			}
