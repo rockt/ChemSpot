2e584e9cc69e324bdb641161c679153b8d6c3cf6
thuber <thuber@informatik.hu-berlin.de>
2013-04-02 18:29:37 +0200

    sanitized and optimized token converter

--- src/main/java/de/berlin/hu/uima/util/OpenNLPToUCompareTokenConverterAE.java
+++ src/main/java/de/berlin/hu/uima/util/OpenNLPToUCompareTokenConverterAE.java
@@ -3,36 +3,28 @@ package de.berlin.hu.uima.util;
 import opennlp.uima.Token;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.FSIndex;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
+import org.uimafit.util.JCasUtil;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class OpenNLPToUCompareTokenConverterAE extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		FSIndex<Annotation> tokenIndex = aJCas.getAnnotationIndex(Token.type);
-		Iterator<Annotation> tokenIterator = tokenIndex.iterator();
+		Iterator<org.u_compare.shared.syntactic.Token> uCompareTokenIterator = JCasUtil.iterator(aJCas, org.u_compare.shared.syntactic.Token.class);
+		for (Token token : JCasUtil.iterate(aJCas, opennlp.uima.Token.class)) {
+			org.u_compare.shared.syntactic.Token uCompareToken = uCompareTokenIterator.next();
+			
+			uCompareToken.setLabel(token.getPos());
+		}
 		
-		List<Annotation> oldTokens = new ArrayList<Annotation>();
-		for (Annotation token : aJCas.getAnnotationIndex(org.u_compare.shared.syntactic.Token.type)) {
+		/*List<Annotation> oldTokens = new ArrayList<Annotation>();
+		for (Annotation token : JCasUtil.iterate(aJCas, opennlp.uima.Token.class)) {
 			oldTokens.add(token);
 		}
 		for (Annotation oldToken : oldTokens) {
-			oldToken.removeFromIndexes(aJCas);
-		}
-
-		while (tokenIterator.hasNext()) {
-			Token token = (Token) tokenIterator.next();
-			org.u_compare.shared.syntactic.Token ucompareToken = new org.u_compare.shared.syntactic.Token(aJCas);
-			ucompareToken.setBegin(token.getBegin());
-			ucompareToken.setEnd(token.getEnd());
-			ucompareToken.setLabel(token.getPos());
-			ucompareToken.addToIndexes();
-		}
+			oldToken.removeFromIndexes();
+		}*/
 	}
 }
