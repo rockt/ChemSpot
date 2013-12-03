d13d7696012fa36befb14b103d1d54daa1ba46d1
thuber@informatik.hu-berlin.de <thuber@informatik.hu-berlin.de>
2013-12-03 12:01:42 +0100

    adding fix for missing sentence annotations

--- src/main/java/de/berlin/hu/uima/util/OpenNLPToUCompareSentenceConverterAE.java
+++ src/main/java/de/berlin/hu/uima/util/OpenNLPToUCompareSentenceConverterAE.java
@@ -3,9 +3,10 @@ package de.berlin.hu.uima.util;
 import opennlp.uima.Sentence;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.FSIndex;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
+import org.uimafit.util.JCasUtil;
+
+import de.berlin.hu.types.PubmedDocument;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -14,22 +15,39 @@ public class OpenNLPToUCompareSentenceConverterAE extends JCasAnnotator_ImplBase
 
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		FSIndex<Annotation> sentenceIndex = aJCas.getAnnotationIndex(Sentence.type);
-		Iterator<Annotation> sentenceIterator = sentenceIndex.iterator();
-		
-		ArrayList<Sentence> sentencesToRemove = new ArrayList<Sentence>();
-		
-		while (sentenceIterator.hasNext()) {
-			Sentence sentence = (Sentence) sentenceIterator.next();
-			org.u_compare.shared.syntactic.Sentence ucompareSentence = new org.u_compare.shared.syntactic.Sentence(aJCas);
-			ucompareSentence.setBegin(sentence.getBegin());
-			ucompareSentence.setEnd(sentence.getEnd());
-			ucompareSentence.addToIndexes();
-			sentencesToRemove.add(sentence);
-		}
-		
-		for (Sentence sentence : sentencesToRemove) {
-			sentence.removeFromIndexes(aJCas);
+		for (PubmedDocument document : JCasUtil.iterate(aJCas, PubmedDocument.class)) {
+			Iterator<Sentence> sentenceIterator = JCasUtil.selectCovered(aJCas, Sentence.class, document).iterator();
+			
+			ArrayList<Sentence> sentencesToRemove = new ArrayList<Sentence>();
+			
+			if (!sentenceIterator.hasNext()) {
+				//System.out.println("Document does not have any sentences. Adding sentence for entire document.");
+				org.u_compare.shared.syntactic.Sentence ucompareSentence = new org.u_compare.shared.syntactic.Sentence(aJCas);
+				ucompareSentence.setBegin(0);
+				ucompareSentence.setEnd(aJCas.getDocumentText().length());
+				ucompareSentence.addToIndexes();
+			}
+			
+			Sentence sentence = null;
+			while (sentenceIterator.hasNext()) {
+				sentence = sentenceIterator.next();
+				org.u_compare.shared.syntactic.Sentence ucompareSentence = new org.u_compare.shared.syntactic.Sentence(aJCas);
+				ucompareSentence.setBegin(sentence.getBegin());
+				ucompareSentence.setEnd(sentence.getEnd());
+				ucompareSentence.addToIndexes();
+				sentencesToRemove.add(sentence);
+			}
+			if (sentence != null && sentence.getEnd() < document.getEnd() && !document.getCoveredText().substring(sentence.getEnd()+1 - document.getBegin()).matches("\\s*")) {
+				//System.out.println("Sentences do not cover entire document. Adding sentence add the end.");
+				org.u_compare.shared.syntactic.Sentence ucompareSentence = new org.u_compare.shared.syntactic.Sentence(aJCas);
+				ucompareSentence.setBegin(sentence.getEnd()+1);
+				ucompareSentence.setEnd(aJCas.getDocumentText().length());
+				ucompareSentence.addToIndexes();
+			}
+			
+			for (Sentence sentence2 : sentencesToRemove) {
+				sentence2.removeFromIndexes(aJCas);
+			}
 		}
 	}
 
